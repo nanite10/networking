0d463bac4efce897970e9ab24227962cc32546cb
nanite10 <44302569+nanite10@users.noreply.github.com>
2020-01-23 23:31:36 -0500

    Create install_ofed.sh

---
+++ install_ofed.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yum makecache
+kernel_version=`uname -r`
+yum install rsync perl createrepo kernel-headers-$kernel_version python-devel pciutils lsof kernel-devel-$kernel_version redhat-rpm-config rpm-build gcc -y
+if ! [ -e /tmp/mlnx_ofed/mlnxofedinstall ]; then echo "ERROR: Could not find OFED installer /tmp/mlnx_ofed/mlnxofedinstall"; exit 1; fi
+/tmp/mlnx_ofed/mlnxofedinstall --without-32bit --skip-distro-check --add-kernel-support --kmp --enable-mlnx_tune --enable-affinity --enable-opensm --all --upstream-libs --force
